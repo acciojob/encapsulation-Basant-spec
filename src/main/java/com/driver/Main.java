38d0783b79129a1cc8c92764df573e44ecd3acaa
Basant <basantdhiman9@gmail.com>
2024-08-13 13:13:32 +0530

    check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj=new RWOnly();
-        obj.setName("Basant");
+        obj.setName("BasantDhiman");
         System.out.print(obj.getName());
 
     }
